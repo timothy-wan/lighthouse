9bd7e2b8d558f5d911fa29a1762697232a92ec90
Tim <tw5033@hotmail.com>
2019-04-04 19:46:23 +0000

    custom map function completed

---
+++ w1d4/custom-map.js
@@ -0,0 +1,23 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+var map = function(arr, func) {
+  var result = [];
+  arr.forEach(function(elm) {
+    result.push(func(elm));
+  });
+  console.log(result);
+  return result;
+}
+
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
